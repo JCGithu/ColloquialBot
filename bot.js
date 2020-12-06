71236289084feb0dd8a4e0407ba68061747f9632
Jack Gracie <jack.c.gracie@gmail.com>
2020-12-06 14:50:24 +0000

    added roles

--- bot.js
+++ bot.js
@@ -4,6 +4,30 @@ const client = new Discord.Client({
 });
 require('dotenv').config();
 
+const roles = {
+  "Among Us": {
+    "emoji": "🔪",
+    "id": '785120209752293387'
+  },
+  "Age of Mythology":{
+    "emoji": "🔱",
+    "id": "784799109990842398"
+  },
+  "Codenames":{
+    "emoji": "🕵️‍♂️",
+    "id": "785154460929753089"
+  },
+  "D&D":{
+    "emoji":"🐉",
+    "id": "785155885583433728"
+  },
+  "Jackbox Games":{
+    "emoji": "📦",
+    "id": "785155943213301761"
+  }
+
+}
+
 client.on('ready', ()=>{
   console.log('ColloquialBot ready');
 });
@@ -23,8 +47,28 @@ client.on('messageReactionAdd', async (reaction, user) => {
 
   if (user.bot) return;
   if (reaction.message.id == '785132049463115798'){
-    if (reaction.emoji.name === '🔪'){
-      await reaction.message.guild.members.cache.get(user.id).roles.add('785120209752293387');
+    for (role in roles){
+      roleInfo = roles[role];
+      if (reaction.emoji.name === roleInfo.emoji){
+        console.log(user.username + ' ' + role + ' removed')
+        await reaction.message.guild.members.cache.get(user.id).roles.add(roleInfo.id);
+      }
+    }
+  }
+})
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+
+  if (user.bot) return;
+  if (reaction.message.id == '785132049463115798'){
+    for (role in roles){
+      roleInfo = roles[role];
+      if (reaction.emoji.name === roleInfo.emoji){
+        console.log(user.username + ' ' + role + ' removed')
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(roleInfo.id);
+      }
     }
   }
 })
