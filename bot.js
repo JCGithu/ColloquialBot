cd89607878ff4375e0c4e4618deb4fa5ba1e0e15
Jack Gracie <jack.c.gracie@gmail.com>
2020-12-08 22:20:38 +0000

    Rewrite + add FIFA

--- bot.js
+++ bot.js
@@ -1,63 +1,72 @@
+const fs = require('fs');
+const path = require('path');
 const Discord = require('discord.js');
 const client = new Discord.Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
 require('dotenv').config();
 
+const personal = '692853487104688158';
+const twitch = '772398840954748948';
+
 const roles = {
   "Among Us": {
     "emoji": "🔪",
-    "id": '785120209752293387'
+    "id": '785120209752293387',
+    "server": personal
   },
   "Age of Mythology":{
     "emoji": "🔱",
-    "id": "784799109990842398"
+    "id": "784799109990842398",
+    "server": personal
   },
   "Codenames":{
     "emoji": "🕵️‍♂️",
-    "id": "785154460929753089"
+    "id": "785154460929753089",
+    "server": personal
   },
   "D&D":{
     "emoji":"🐉",
-    "id": "785155885583433728"
+    "id": "785155885583433728",
+    "server": personal
   },
   "Jackbox Games":{
     "emoji": "📦",
-    "id": "785155943213301761"
+    "id": "785155943213301761",
+    "server": personal
+  },
+  "FIFA":{
+    "emoji": "⚽",
+    "id": "785993682197151786",
+    "server": personal
   }
-
 }
 
 client.on('ready', ()=>{
   console.log('ColloquialBot ready');
 });
 
+client.on("guildMemberAdd", (user) => {
+  if (user.guild == twitch){
+    user.roles.add('772498414553792522');
+  }
+});
+
 client.on('message', msg =>{
   if (msg.content == 'hello bot!'){
     msg.channel.send('Hi yourself ;)');
   }
-  if (msg.content == '+add among us'){
-    addRole(msg.member,'785120209752293387');
-  }
 })
 
 client.on('messageReactionAdd', async (reaction, user) => {
-  if (reaction.message.partial) await reaction.message.fetch();
-  if (reaction.partial) await reaction.fetch();
-
-  if (user.bot) return;
-  if (reaction.message.id == '785132049463115798'){
-    for (role in roles){
-      roleInfo = roles[role];
-      if (reaction.emoji.name === roleInfo.emoji){
-        console.log(user.username + ' ' + role + ' removed')
-        await reaction.message.guild.members.cache.get(user.id).roles.add(roleInfo.id);
-      }
-    }
-  }
+  roleChange(reaction, user, true);
 })
 
 client.on('messageReactionRemove', async (reaction, user) => {
+  roleChange(reaction, user, false);
+})
+
+async function roleChange(reaction, user, add){
   if (reaction.message.partial) await reaction.message.fetch();
   if (reaction.partial) await reaction.fetch();
 
@@ -65,16 +74,15 @@ client.on('messageReactionRemove', async (reaction, user) => {
   if (reaction.message.id == '785132049463115798'){
     for (role in roles){
       roleInfo = roles[role];
-      if (reaction.emoji.name === roleInfo.emoji){
-        console.log(user.username + ' ' + role + ' removed')
-        await reaction.message.guild.members.cache.get(user.id).roles.remove(roleInfo.id);
+      if (reaction.emoji.name === roleInfo.emoji && roleInfo.server == user.guild){
+        if (add == true){
+          await reaction.message.guild.members.cache.get(user.id).roles.add(roleInfo.id);
+        } else {
+          await reaction.message.guild.members.cache.get(user.id).roles.remove(roleInfo.id);
+        }
       }
     }
   }
-})
-
-function addRole(member, role){
-  member.roles.add(role);
 }
 
 client.login(process.env.BOT_TOKEN);
