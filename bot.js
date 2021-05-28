715d50d07bdc28d38a9f5be30bbfc9967ded0203
jcgithu <jack.c.gracie@gmail.com>
2021-05-28 21:14:57 +0100

    refactor + twitch start

--- bot.js
+++ bot.js
@@ -6,32 +6,38 @@ const client = new Discord.Client({
 });
 require('dotenv').config();
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json());
+app.listen(process.env.PORT || 500, () => console.log('webhook is listening'));
+
+let roles = fs.readFile(path.resolve(__dirname, './roles.json'), async (err, data) => {
+  return JSON.parse(data);
+});
+
 client.on('ready', () => {
   console.log('ColloquialBot ready');
 });
 
 client.on('guildMemberAdd', (user) => {
-  if (user.guild == twitch) {
-    user.roles.add('772498414553792522');
-  }
+  if (user.guild == twitch) user.roles.add('772498414553792522');
 });
 
 client.on('message', (msg) => {
-  if (msg.content == 'hello bot!') {
-    msg.channel.send('Hi yourself ;)');
-  }
-  if (msg.content == '!cheezoid') {
-    let random_boolean = Math.random() < 0.5;
+  if (msg.content === 'hello bot!') msg.channel.send('Hi yourself ;)');
+  if (msg.content === '!cheezoid') {
     let deathName = Math.floor(Math.random() * 10);
-    if (deathName == 3) {
+    if (deathName === 3) {
       msg.channel.send('CHEEZOID ONLY KNOWS PAIN 😟');
-    } else {
-      if (random_boolean == true) {
-        msg.channel.send('CHEESE 🧀');
-      } else {
-        msg.channel.send('PETRIL ⛽');
-      }
+      return;
     }
+    let random_boolean = Math.random() < 0.5;
+    if (random_boolean) {
+      msg.channel.send('CHEESE 🧀');
+      return;
+    }
+    msg.channel.send('PETRIL ⛽');
+    return;
   }
 });
 
@@ -45,33 +51,33 @@ client.on('messageReactionRemove', async (reaction, user) => {
 
 client.on('guildMemberAdd', (member) => {
   console.log('User' + member.user.tag + 'has joined the server!');
-  console.log(member);
 });
 
 async function roleChange(reaction, user, add) {
   if (reaction.message.partial) await reaction.message.fetch();
   if (reaction.partial) await reaction.fetch();
-
   if (user.bot) return;
-  if (reaction.message.id == '785132049463115798' || reaction.message.id == '786549352809758730') {
-    console.log(reaction.emoji.name);
-    fs.readFile(path.resolve(__dirname, './roles.json'), async (err, data) => {
-      if (err) throw err;
-      let roles = JSON.parse(data);
-      for (role in roles) {
-        roleInfo = roles[role];
-        if (reaction.emoji.name === roleInfo.emoji) {
-          if (add == true) {
-            await reaction.message.guild.members.cache.get(user.id).roles.add(roleInfo.id);
-            console.log(user.username + ' added the role ' + role + roleInfo.emoji);
-          } else {
-            await reaction.message.guild.members.cache.get(user.id).roles.remove(roleInfo.id);
-            console.log(user.username + ' removed the role ' + role + roleInfo.emoji);
-          }
+
+  if (reaction.message.id === '785132049463115798' || reaction.message.id === '786549352809758730') {
+    for (role in roles) {
+      roleInfo = roles[role];
+      if (reaction.emoji.name === roleInfo.emoji) {
+        if (add === true) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(roleInfo.id);
+          console.log(`${user.username} added the role ${role} ${roleInfo.emoji}`);
+        } else {
+          await reaction.message.guild.members.cache.get(user.id).roles.remove(roleInfo.id);
+          console.log(`${user.username} removed the role ${role} ${roleInfo.emoji}`);
         }
       }
-    });
+    }
   }
 }
+
+app.post('/webhook', (req, res) => {
+  if (req.body.subscription.type === 'stream.online') {
+    console.log('Stream is online! 📹');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
