12b02bd486075d57a2d3f863f2718ba9e1e01e7d
JCGithu <jack.c.gracie@gmail.com>
2021-11-16 19:40:02 +0000

    adding lurks

---
+++ twitch/colloquialowl/lurk.js
@@ -0,0 +1,31 @@
+let data = [
+"{user} is now venturing into a deep void, known only as the... the Lurk", 
+"{user} has other things to be getting on with!",
+"{user} will be watching. Hidden, but watching.",
+"{user} will be but a whisper on the wind.",
+"{user} will be resting under the Tumtum tree.",
+"{user} melts into shadow.",
+"{user} drives off into the sunset. Maybe we'll see them again one day.",
+"{user}, farewell. We will all miss you dearly.",
+"{user}, although you venture on, we will keep you in our hearts.",
+"{user} is embarking on a quest. Their destination? Why, only fate knows.",
+"{user}'s eyes gloss over, they seem... distant somehow.",
+"{user} hears a calling. They must leave this planet, their people need them.",
+"{user} smiles, and vanishes in a flash of blinding light.",
+"{user} is lurking. What? They don't all have to be creative.",
+"{user} will return...",
+"{user} has other buisness to attend to.",
+`{user} points in the distance. "What's that?!". You turn to look, but there's nothing. They've vanished.`,
+"{user} turns to smoke, their last smile curling into the wind.",
+"{user} has a very important meeting to attend.",
+"{user} can't take this any longer. ColloquialOwl is just too alluring. They think they need some space.",
+"{user} will be back when we need them most."
+];
+
+const getRandomInt = require("../../tools/getRandomInt")
+module.exports = (channel, tags, message, client) => {
+    let user = tags.username
+    let quote = data[getRandomInt(data.length)]
+    quote.replace('{user}', user);
+    return quote;
+};
---
+++ twitch/colloquialowl/unlurk.js
@@ -0,0 +1,26 @@
+let data = [
+    "{user} has returned from the darkness",
+    "{user} came whiffling through the tulgey wood, and burbled as it came!",
+    "{user}? Why, that's a username I haven't heard in a long time... *takes drag on pipe*",
+    "{user}! Here! In this stream?!",
+    "{user} returned when we needed them most",
+    "{user}! You've grown so much, look at you!",
+    "{user}! Long time no see you old fool.",
+    "{user}, by the gods, is that really you?",
+    "{user} emerges triumphant.", 
+    "{user} returns. *Inception Sound blares*",
+    "{user} comes bearing gifts",
+    "{user} bursts through the door.",
+    "{user} hesitates. Will they really be welcomed here, as though no time has passed at all?",
+    "{user} is back and badder than ever",
+    "{user} asks what year it is. It's been so long they were trapped in the jungle themed board game.",
+    "{user} has reentered the arena."
+];
+
+const getRandomInt = require("../../tools/getRandomInt")
+module.exports = (channel, tags, message, client) => {
+    let user = tags.username
+    let quote = data[getRandomInt(data.length)]
+    quote.replace('{user}', user);
+    return quote;
+};
