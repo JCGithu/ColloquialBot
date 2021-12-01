bd315a4a81f84816888273c5cfc253f088d7adb9
JCGithu <jack.c.gracie@gmail.com>
2021-12-01 15:13:32 +0000

    new commands

---
+++ twitch/colloquialowl/chatter.js
@@ -0,0 +1,5 @@
+async function run(){
+  return 'The chatbox is a tool we made on stream! You can get it yourself at https://colloquial.studio/chatter'
+}
+
+module.exports = run;
---
+++ twitch/colloquialowl/emotedrop.js
@@ -0,0 +1,5 @@
+async function run(){
+  return 'Emote Drop is a tool we made on stream! You can get it yourself at https://colloquial.studio/emotedrop'
+}
+
+module.exports = run;
