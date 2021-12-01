246507bf77d163e5c36ac0d5db576eeae428ac66
JCGithu <jack.c.gracie@gmail.com>
2021-12-01 15:18:41 +0000

    added command

---
+++ twitch/colloquialowl/colloquialbot.js
@@ -0,0 +1,3 @@
+async function run(){
+  return "Colloquialbot 🤖 runs 24/7 on Heroku on Twitch and Discord, if you're a friend he can run on your chat too! If you want to peek inside his code is on the github."
+}
