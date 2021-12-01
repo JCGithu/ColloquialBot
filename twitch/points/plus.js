e9b73ea7492be6cc88fb64fa1bd6379b9291a7bd
JCGithu <jack.c.gracie@gmail.com>
2021-12-01 15:13:56 +0000

    fixed point system

--- twitch/points/plus.js
+++ twitch/points/plus.js
@@ -4,9 +4,8 @@ async function plus(channel, tags, message, client, ComfyDB) {
     if (tags.username === target) return `${tags.username} just tried to give THEMSELVES a point...`;
     let nameCheck = await ComfyDB.Get(target);
     if (!nameCheck) return `${target} isn't in the database!`;
-    await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: message } } } );
+    await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: target } } } );
     return `${tags.username} just gave ${target} a point! They now have ${nameCheck.points + 1} points`;
 };
-
 
 module.exports = plus;
--- twitch/points/rerank.js
+++ twitch/points/rerank.js
@@ -1,9 +1,9 @@
 async function reRank(channel, tags, message, client, ComfyDB){
-    if (!tags.badges.broadcaster) return 'Only the broadcaster can use this command.';
+    if (!tags.badges.broadcaster && !tags.badges.moderator) return 'Only the broadcaster/mods can use this command.';
     let sort = await ComfyDB.Search({ sortBy: "points", sort: "desc"});
     for (let result in sort){
         let newRank = parseInt(result) + 1;
-        await ComfyDB.Store( sort[result].username, { rank: newRank});
+        await ComfyDB.Store( sort[result].username, {rank: newRank});
     }
     return `Top 100 users ranked!`;
 };
