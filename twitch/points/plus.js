01dd5e248f017a7637e26abb7e8a435b85626e3e
JCGithu <jack.c.gracie@gmail.com>
2021-12-16 23:25:45 +0000

    acorn mod added

--- twitch/points/plus.js
+++ twitch/points/plus.js
@@ -3,7 +3,7 @@ async function plus(channel, tags, message, client, ComfyDB) {
     let target = message.split(' ')[0].toLowerCase();
     if (tags.username === target) return `${tags.username} just tried to give THEMSELVES an acorn...`;
     let nameCheck = await ComfyDB.Get(target);
-    if (!nameCheck) return `${target} isn't in the database!`;
+    if (!nameCheck) return `${target} isn't in the database! Mods please refund`;
     await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: target } } } );
     return `${tags.username} just gave ${target} an acorn! They now have ${nameCheck.points + 1} acorns`;
 };
