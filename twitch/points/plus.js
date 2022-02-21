e9513f72d2bd9a77416cd5bd0520164c29b70e5a
JCGithu <jack.c.gracie@gmail.com>
2022-02-21 21:52:10 +0000

    removing the need to add to db

--- twitch/points/plus.js
+++ twitch/points/plus.js
@@ -3,7 +3,10 @@ async function plus(channel, tags, message, client, ComfyDB) {
     let target = message.split(' ')[0].toLowerCase();
     if (tags.username === target) return `${tags.username} just tried to give THEMSELVES an acorn...`;
     let nameCheck = await ComfyDB.Get(target);
-    if (!nameCheck) return `${target} isn't in the database! Mods please refund`;
+    if (!nameCheck) {
+        await ComfyDB.Store( tags.username, { username: tags.username, bio: ' ', points: 1 } );
+        return `${target} was given their first acorn!`
+    }
     await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: target } } } );
     return `${tags.username} just gave ${target} an acorn! They now have ${nameCheck.points + 1} acorns`;
 };
