cb32d34b5ef6c0c7eeaa15b88051b6b8a6258323
JCGithu <jack.c.gracie@gmail.com>
2022-02-21 23:55:07 +0000

    added tiny to discord

---
+++ discord/colloquialowl/commands/tiny.js
@@ -0,0 +1,9 @@
+const tiny = require('tiny-text');
+
+async function run(message, ComfyDB){
+    message = message.replace('!tiny', "");
+    let output = tiny(message);
+    return `Here's your order! ${output}`;
+}
+
+module.exports = run;
