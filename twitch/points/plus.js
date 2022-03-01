877b8499645d5172a031e520bfca81202adf8f6c
JCGithu <jack.c.gracie@gmail.com>
2022-03-01 01:36:35 +0000

    missing another thing

--- twitch/points/plus.js
+++ twitch/points/plus.js
@@ -7,7 +7,7 @@ async function plus(channel, tags, message, client, ComfyDB) {
     let nameCheck = await ComfyDB.Get(target);
     if (!nameCheck) await addPerson(channel, tags, message, client, ComfyDB);
     await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: target } } } );
-    return `${tags.username} just gave ${target} an acorn! They now have ${nameCheck.points + 1} acorns`;
+    return `${tags.username} just gave ${target} an acorn! They now have ${(nameCheck.points + 1) + ' acorns'|| 'their first acorn!'}`;
 };
 
 module.exports = plus;
