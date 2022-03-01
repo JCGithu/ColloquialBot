328a39c5e9de455b0724627c604a7ada1e551217
JCGithu <jack.c.gracie@gmail.com>
2022-03-01 01:30:12 +0000

    acorn patching

--- twitch/points/me.js
+++ twitch/points/me.js
@@ -2,10 +2,10 @@ async function addPerson(channel, tags, message, client, ComfyDB) {
     let nameCheck = await ComfyDB.Get(tags.username);
     if (nameCheck) return `${tags.username} is already in the database!`;
     if (message === '+me'){
-        await ComfyDB.Store( tags.username, { username: tags.username, bio: ' ', points: 0 } );
+        await ComfyDB.Store( tags.username, { username: tags.username, points: 0 } );
         return `${tags.username} has been added!`
     }
-    await ComfyDB.Store( tags.username, { username: tags.username, bio: message.substring(4), points: 0 } );
+    await ComfyDB.Store( tags.username, { username: tags.username, points: 0 } );
     return `${tags.username} has been added!`;
 }
 
--- twitch/points/plus.js
+++ twitch/points/plus.js
@@ -1,12 +1,11 @@
+const addPerson = require('./me.js')
+
 async function plus(channel, tags, message, client, ComfyDB) {
     if (message.charAt(0) === '@') message = message.substring(1);
     let target = message.split(' ')[0].toLowerCase();
     if (tags.username === target) return `${tags.username} just tried to give THEMSELVES an acorn...`;
     let nameCheck = await ComfyDB.Get(target);
-    if (!nameCheck) {
-        await ComfyDB.Store( tags.username, { username: tags.username, bio: ' ', points: 1 } );
-        return `${target} was given their first acorn!`
-    }
+    if (!nameCheck) await addPerson(channel, tags, message, client, ComfyDB);
     await ComfyDB.Increment( "points", { by: 1, where: { username: { equals: target } } } );
     return `${tags.username} just gave ${target} an acorn! They now have ${nameCheck.points + 1} acorns`;
 };
