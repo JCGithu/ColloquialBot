63f1e671346a36b2717c41263b3d874995d20774
JCGithu <jack.c.gracie@gmail.com>
2022-03-20 18:43:54 +0000

    updating discord live messages

--- twitch/colloquialowl/notify.js
+++ twitch/colloquialowl/notify.js
@@ -5,19 +5,24 @@ let data = [
     "1.2. 1.2. Is this this on?",
     "Ah ah ah ah starting a live, starting a live",
     "This is not a drill",
-    "Live & vibe",
+    "Live-ing & vibing",
     "Your friendly neighbourhood Discord bot would like you to check out the stream please!",
     "10 Secrets of how to watch this livesteam. Number 6 will shock you!",
     "like comment subscribe",
     "Where we're going, we don't need streams",
     "Hahaha oh what's that? You just caught me watching this really funny stream, you can check it out here.",
-    "Stack Overflow Any% Speedrun",
+    "I am programmed to generate hype. Hype generated.",
     "ColloquialOwl is filmed in front of a live studio audience",
     "No Owls are harmed in the production of this stream",
     "Hello fellow kids can I get a sheesh for this cringfest over here lmao",
     "[90's reference]",
     "*Cue applause*",
-    "Breaking: Man breaks down on livestream"
+    "Breaking: Man breaks down on livestream",
+    "Woah, just found this stream I think you'd like xXxXx",
+    "Tell this guy 'colloquialbot sent me' for a special discount, he knows the drill",
+    "This generated message demands your attention",
+    "In my own lane, thriving",
+    "Will the audio levels be right? Find out this week on the latest episode... "
 ]
 
 async function run(channel, tags, message, client){
