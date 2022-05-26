0410a23e0c883e4cb0ca62d366d11659c771ca18
JCGithu <jack.c.gracie@gmail.com>
2022-05-26 23:05:30 +0100

    fix potential tiny crash

--- twitch/colloquialowl/tiny.js
+++ twitch/colloquialowl/tiny.js
@@ -2,6 +2,7 @@ const tiny = require('tiny-text');
 
 async function run(channel, tags, message, client){
     message = message.replace('!tiny', "");
+    if (!message.length) return '/me No message to tiny :(';
     let output = tiny(message);
     return `Here's your order! ${output}`;
 }
