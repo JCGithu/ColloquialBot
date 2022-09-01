5918d2ba016b85d6d7927b093381514f92760604
JCGithu <jack.c.gracie@gmail.com>
2022-09-01 10:28:22 +0100

    patching uwu

--- twitch/arcasian/uwu.js
+++ twitch/arcasian/uwu.js
@@ -7,7 +7,71 @@ var kaomojiEmbarassed = [" (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)..", " (*^.^*)..,", "..,
 var kaomojiConfuse    = [" (o_O)?", " (°ロ°) !?", " (ーー;)?", " owo?"];
 var kaomojiSparkles   = [" *:･ﾟ✧*:･ﾟ✧ ", " ☆*:・ﾟ ", "〜☆ ", " uguu.., ", "-.-"];
 
-function uwuinate(text) {
+function uwuinate() {
+  let
+  function uwuWord(word, index) {
+    var uwu = "";
+  
+    var lastChar = word[word.length - 1];
+    var end = "";
+    var random = 0;
+    if (lastChar == '.' || lastChar == '?' || lastChar == '!' || lastChar == ',') {
+      word = word.slice(0, -1);
+      end = lastChar;
+      if (end === '.' || end === '!') end = kaomojiJoy[getRandomInt(kaomojiJoy.length)];
+      if (end === '?') end = kaomojiConfuse[getRandomInt(kaomojiConfuse.length)];
+      if (end == ',') end = kaomojiEmbarassed[getRandomInt(kaomojiEmbarassed.length)];
+  
+      let sparkle = getRandomInt(1);
+      if (sparkle) end = end + kaomojiSparkles[getRandomInt(kaomojiSparkles.length)];
+    }
+  
+    if (word.indexOf('l') > -1) {
+      if (word.slice(-2) == "le" || word.slice(-2) == "ll") {
+        uwu += word.slice(0, -2).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-2) + end + ' ';
+      }
+      else if (word.slice(-3) == "les" || word.slice(-3) == "lls") {
+        uwu += word.slice(0, -3).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-3) + end + ' ';
+      }
+      else {
+        uwu += word.replace(/l/g, 'w').replace(/r/g, 'w') + end + ' ';
+      }
+    }
+    else if (word.indexOf('r') > -1) {
+      if (word.slice(-2) == "er" || word.slice(-2) == "re") {
+        uwu += word.slice(0, -2).replace(/r/g, 'w') + word.slice(-2) + end + ' ';
+      }
+      else if (word.slice(-3) == "ers" || word.slice(-3) == "res") {
+        uwu += word.slice(0, -3).replace(/r/g, 'w') + word.slice(-3) + end + ' ';
+      }
+      else {
+        uwu += word.replace(/r/g, 'w') + end + ' ';
+      }
+    }
+    else {
+      uwu += word + end + ' ';
+    }
+  
+    if (word === '*'){
+      uwu += kaomojiSparkles[getRandomInt(kaomojiSparkles.length)] + end + ' ';
+    }
+  
+    uwu = uwu.replace(/you're/g, "ur");
+    uwu = uwu.replace(/youre/g, "ur");
+    uwu = uwu.replace(/shit/g, "poopoo");
+    uwu = uwu.replace(/asshole/g, "b-butthole");
+    uwu = uwu.replace(/\bdad\b/g, "daddy");
+    uwu = uwu.replace(/\bfather\b/g, "daddy");
+    uwu = uwu.replace(/\bcute\b/g, "cutie ;)");
+  
+    //Stutter
+    if (uwu.length > 2 && uwu[0].match(/[a-z]/i)) {
+      random = Math.floor(Math.random() * 5);
+      if (random == 0) uwu = uwu[0] + '-' + uwu;
+    }
+    return uwu;
+  }
+
   if (!text) return false;
   text = text.replace('!uwu','');
   text = text.toLowerCase();
@@ -17,70 +81,5 @@ function uwuinate(text) {
   return finalText;
 }
 
-function uwuWord(word, index) {
-  var uwu = "";
-
-  var lastChar = word[word.length - 1];
-  var end = "";
-  var random = 0;
-  if (lastChar == '.' || lastChar == '?' || lastChar == '!' || lastChar == ',') {
-    word = word.slice(0, -1);
-    end = lastChar;
-    if (end === '.' || end === '!') end = kaomojiJoy[getRandomInt(kaomojiJoy.length)];
-    if (end === '?') end = kaomojiConfuse[getRandomInt(kaomojiConfuse.length)];
-    if (end == ',') end = kaomojiEmbarassed[getRandomInt(kaomojiEmbarassed.length)];
-
-    let sparkle = getRandomInt(1);
-    if (sparkle) end = end + kaomojiSparkles[getRandomInt(kaomojiSparkles.length)];
-  }
-
-  if (word.indexOf('l') > -1) {
-    if (word.slice(-2) == "le" || word.slice(-2) == "ll") {
-      uwu += word.slice(0, -2).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-2) + end + ' ';
-    }
-    else if (word.slice(-3) == "les" || word.slice(-3) == "lls") {
-      uwu += word.slice(0, -3).replace(/l/g, 'w').replace(/r/g, 'w') + word.slice(-3) + end + ' ';
-    }
-    else {
-      uwu += word.replace(/l/g, 'w').replace(/r/g, 'w') + end + ' ';
-    }
-  }
-  else if (word.indexOf('r') > -1) {
-    if (word.slice(-2) == "er" || word.slice(-2) == "re") {
-      uwu += word.slice(0, -2).replace(/r/g, 'w') + word.slice(-2) + end + ' ';
-    }
-    else if (word.slice(-3) == "ers" || word.slice(-3) == "res") {
-      uwu += word.slice(0, -3).replace(/r/g, 'w') + word.slice(-3) + end + ' ';
-    }
-    else {
-      uwu += word.replace(/r/g, 'w') + end + ' ';
-    }
-  }
-  else {
-    uwu += word + end + ' ';
-  }
-
-  if (word === '*'){
-    uwu += kaomojiSparkles[getRandomInt(kaomojiSparkles.length)] + end + ' ';
-  }
-
-  uwu = uwu.replace(/you're/g, "ur");
-  uwu = uwu.replace(/youre/g, "ur");
-  uwu = uwu.replace(/shit/g, "poopoo");
-  uwu = uwu.replace(/asshole/g, "b-butthole");
-  uwu = uwu.replace(/\bdad\b/g, "daddy");
-  uwu = uwu.replace(/\bfather\b/g, "daddy");
-  uwu = uwu.replace(/\bcute\b/g, "cutie ;)");
-
-  //Stutter
-  if (uwu.length > 2 && uwu[0].match(/[a-z]/i)) {
-    random = Math.floor(Math.random() * 5);
-    if (random == 0) uwu = uwu[0] + '-' + uwu;
-  }
-  return uwu;
-}
-
-async function run(query){
-  return uwuinate(query);
-}
+uwuinate();
 
